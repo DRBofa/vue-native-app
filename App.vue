2b22c9e74dd7d5a5e2f6ab290fe90a1ef1cb94f8
BonaOng <bonasmart9@gmail.com>
2019-02-10 23:39:53 +0700

    firt app

---
+++ App.vue
@@ -0,0 +1,40 @@
+<template>
+<view class="container">
+    <text class="text-color-primary">{{message}}</text>
+    <text-input
+        class="text-color-primary"
+        :style="{height: 40, borderColor: 'gray', borderWidth: 1}"
+        v-model="message" />
+   
+    <button v-bind:title="message" v-bind:on-press="handleBtnPress"/>
+  </view>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            message: "Hello World"
+        };
+
+    },
+    methods: {
+        handleBtnPress: function () {
+            alert('Btn Press');
+        }
+    }
+};
+</script>
+
+<style>
+.container {
+    background-color: white;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+}
+
+.text-color-primary {
+    color: blue;
+}
+</style>
